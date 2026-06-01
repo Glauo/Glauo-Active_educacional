58803fa3a092ec214518abd76f54219136673491
Glauco <gllavolpini@gmail.com>
2026-06-01 12:50:59 -0300

    fix: isolate active docker service aliases

--- deploy/wizbot/server.py
+++ deploy/wizbot/server.py
@@ -971,7 +971,7 @@ def _get_frontend_base_url():
     if url:
         return url.rstrip("/")
     # Nome do servico na rede interna do Docker Compose
-    return "http://app:3000"
+    return "http://ativo_educacional_app:3000"
 
 
 def _cron_regerar_boletos():
